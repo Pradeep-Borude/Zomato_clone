fcf1873ac1462cdc0faa0b0062f99f258573bba0
procoder406 <pradeepborude406@gmail.com>
2025-11-19 14:29:18 +0530

    Removed sensitive data from commit history

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes');
-
+const foodRoutes = require('./routes/food.routes')
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -10,4 +10,6 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 })
 app.use('/api/auth',authRoutes)
+app.use('/api/food',foodRoutes)
 module.exports = app
+
